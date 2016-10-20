a373328f7ceaae9934cb39d75e30f79570d0fb6c
samnags <sam.nagourney@gmail.com>
2016-10-20 07:23:38 -0400

    actions added

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-let store = createStore(todoApp)
+// let store = createStore(todoApp)
 
 
 ReactDOM.render(
-  <Provider store={}
   <App />,
   document.getElementById('root')
 );
--- src/reducers/todo_reducer.js
+++ src/reducers/todo_reducer.js
@@ -1,4 +1,44 @@
-function (state = null, action) {
-  case action.type
+import { VisibilityFilters } from './actions/'
+
+const initialState = {
+  visibilityFilter: VisibilityFilters.SHOW_ALL,
+  todos: []
+}
+
+function todos (state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false
+        }
+      ]
+
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, state, {
+              completed: !todo.completed
+            })
+          }
+          return todo
+        }
+      })
+    default:
+      return state
+  }
+
+function todoApp(state = initialState, action) {
 
 }
+
+
+
+
+
+case SET_VISIBILITY_FILTER:
+return Object.assign({}, state, {
+  visibilityFilter: action.filter
+})
